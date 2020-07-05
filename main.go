b5f487549c3f6d1e24adee517e4acfa8eae1db96
Rean <zl@sunmi.com>
2020-07-05 19:06:49 +0800

    feat:rename

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
     "fmt"
+    "github.com/reaburoa/logger/logger"
 )
 
 func main() {
     fmt.Println("logger library ...")
+    logger.InitLogger("sss", "./Runtime", "json", 1, 1, 2, true, true, true)
+    logger.Sugar.Info("qqqq")
 }
